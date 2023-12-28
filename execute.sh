784b653913a54525b9a9d647cdb65faa5be4bdf7
Luis Cañas-Díaz <lcanas@bitergia.com>
2023-12-28 22:28:48 +0100

    f

--- execute.sh
+++ execute.sh
@@ -3,4 +3,7 @@
 python3 -m venv ./virtual-env
 source ./virtual-env/bin/activate
 pip install -r requirements.txt -q
-python clean_up_indices.py
+for fname in `ls configurations`
+do
+  python clean_up_indices.py ./configurations/$fname
+done
